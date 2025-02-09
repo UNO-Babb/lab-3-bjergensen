16bfc43c980b68acec2bac2b7278afee7096ad0c
bjergensen <bjergensen@unomaha.edu>
2025-02-09 23:17:16 +0000

    Finished

--- RPS.py
+++ RPS.py
@@ -1,7 +1,7 @@
 #RPS.py
-#Name:
-#Date:
-#Assignment:
+#Name: Brandon Jergensen
+#Date: 2/9/25
+#Assignment: RPS.py
 import random
 
 def main():
@@ -9,11 +9,51 @@ def main():
   ties = 0
   losses = 0
   #Create a loop that continues as long as the user wants to play.
+  while True: 
   #User can play as many games as they wish.
 
   #Randomly choose the computer between 'R', 'P', or 'S'
+    computer = random.choice( ["R", "P", "S"])
+  
   #Prompt the user for their RPS selection
+    player = input("pick your weapon (R, P, S):")
+
+    if player not in ["R", "P", "S"]:
+      print("Quit playin' and pick R, P, or S.")
+
   #Determine winner and state what happened to the user
+    if computer =="R":
+      print ("Computer Chose Rock")
+    elif computer =="P":
+      print ("Computer chose Paper")
+    else:
+      print ("Computer chose Scissors")
+
+    if player == computer:
+      print("Tie")
+      ties = ties + 1
+    if player == "R" and computer == "S":
+      print("You win!")
+      wins = wins + 1
+    if player == "P" and computer == "R":
+      print("You win!")
+      wins = wins + 1
+    if player == "S" and computer == "P":
+      print("You win!")
+      wins = wins + 1
+    if player == "R" and computer == "P":
+      print("You lose!")
+      losses = losses + 1
+    if player == "P" and computer == "S":
+      print("You lose!")
+      losses = losses + 1
+    if player == "S" and computer == "R":
+      print("You lose!")
+      losses = losses + 1
+
+    play_again = input("Would you like to play again? (Y/N): ").upper()
+    if play_again != "Y":
+      break
   #Ask the user if they would like to play again.
 
   #In the end, print the stats
--- TempConvert.py
+++ TempConvert.py
@@ -1,16 +1,19 @@
 #TempConvert.py
-#Name:
-#Date:
-#Assignment:
+#Name: Brandon Jergensen
+#Date: 2/9/25
+#Assignment: TempConvert.py
 
 
 def main():
   #Prompt the user for a Fahrenheit temperature
   #Convert that temperature to celsius, rounding to 1 decimal percision
   #Output converted temperature.
-  tempF = 80
+  tempF = int(float(input("What is the temperature in Fahrenheit?:")))
 
-  tempC = tempF / 2
+  tempC = round((tempF - 32) * 5 / 9)
+
+
+  
 
   print(tempF, "is ", tempC, "degrees celsius.")
 if __name__ == '__main__':
